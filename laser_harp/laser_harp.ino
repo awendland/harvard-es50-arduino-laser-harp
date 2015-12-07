f129085e8baec09fd1a0be2a253f48a4c3230582
Alex Wendland <me@alexwendland.com>
2015-12-06 20:52:56 -0500

    Update to support 16 total feeds

--- laser_harp/laser_harp.ino
+++ laser_harp/laser_harp.ino
@@ -5,11 +5,12 @@
  mapping of notes for the NOTES constant:
  \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
     const START = 0x5A;
-    const SIDE_A = 6;
+    const SIDE_A = 8;
+    const SIDE_B = 8;
 
     let notes = [];
 
-    for (let i = 0; i < 36; ++i) {
+    for (let i = 0; i < SIDE_A * SIDE_B; ++i) {
         if (i % SIDE_A === 0) notes.push([]);
         notes[notes.length - 1].push(
                 "0x" + (START + i)
@@ -30,23 +31,25 @@
 #include <SoftwareSerial.h>
 
 // Constants
-#define NUM_CHANNELS 3
-static const uint8_t CHANNEL_PINS[] = {8,9,10};
+#define NUM_CHANNELS 4
+static const uint8_t CHANNEL_PINS[] = {7,8,9,10};
 #define NUM_FEEDS_PER_CHANNEL 4
 #define TOTAL_FEEDS NUM_CHANNELS * NUM_FEEDS_PER_CHANNEL
-#define NUM_FEEDS_SIDE_A 6
+#define NUM_FEEDS_SIDE_A 8
 #define NUM_FEEDS_SIDE_B TOTAL_FEEDS - NUM_FEEDS_SIDE_A
 #define NUM_INTERSECTIONS NUM_FEEDS_SIDE_A * NUM_FEEDS_SIDE_B
-static const uint8_t FEED_PINS[] = {A0,A1,A2,A3};
+static const uint8_t FEED_PINS[] = {A0,A1,A2,A3,A4};
 #define LASER_CNTRL_PIN 11
 #define MIDI_PIN 3
 static const uint8_t NOTES[] = {
-  0x5A, 0x5B, 0x5C, 0x5D, 0x5E, 0x5F,
-  0x60, 0x61, 0x62, 0x63, 0x64, 0x65,
-  0x66, 0x67, 0x68, 0x69, 0x6A, 0x6B,
-  0x6C, 0x6D, 0x6E, 0x6F, 0x70, 0x71,
-  0x72, 0x73, 0x74, 0x75, 0x76, 0x77,
-  0x78, 0x79, 0x7A, 0x7B, 0x7C, 0x7D
+  0x1E, 0x1F, 0x20, 0x21, 0x22, 0x23, 0x24, 0x25,
+  0x26, 0x27, 0x28, 0x29, 0x2A, 0x2B, 0x2C, 0x2D,
+  0x2E, 0x2F, 0x30, 0x31, 0x32, 0x33, 0x34, 0x35,
+  0x36, 0x37, 0x38, 0x39, 0x3A, 0x3B, 0x3C, 0x3D,
+  0x3E, 0x3F, 0x40, 0x41, 0x42, 0x43, 0x44, 0x45,
+  0x46, 0x47, 0x48, 0x49, 0x4A, 0x4B, 0x4C, 0x4D,
+  0x4E, 0x4F, 0x50, 0x51, 0x52, 0x53, 0x54, 0x55,
+  0x56, 0x57, 0x58, 0x59, 0x5A, 0x5B, 0x5C, 0x5D
 };
 
 // Variables
@@ -186,5 +189,5 @@ void playNote(int cmd, int pitch, int velocity) {
   midiSerial.write(pitch);
   midiSerial.write(velocity);
   
-  printf(128, "cmd: %u, pitch: %u, velocity: %u", cmd, pitch, velocity);
+  printf(128, "   cmd: %u, pitch: %u, velocity: %u", cmd, pitch, velocity);
 }
